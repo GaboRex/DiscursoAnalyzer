acf65b1b10194355b67ad591fb9c04270e7b70ef
Gabriel Neme <gaboneme11@gmail.com>
2023-12-19 00:45:14 -0400

    Add Readme and fix notebook

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# DiscursoAnalyzer
+# IDiscurso 🗣️🥸
+
+## Deploy nube☁️
+
+        https://segundoparcial-topicsia-ygh66irtda-uc.a.run.app/docs
+
+## Descripción 📚🔍
+IDiscurso es una API diseñada para analizar archivos de texto y determinar si son discursos. Acepta archivos en formato .txt y .docx, y utiliza un modelo de lenguaje para extraer los embeddings del texto. Luego, compara estos embeddings con los embeddings precalculados de tres tipos de textos: discursos, noticias y canciones.
+
+## Propósito 🎯
+El propósito principal de IDiscurso es analizar archivos de texto y proporcionar información sobre si el texto es un discurso, así como detalles específicos sobre los textos más similares.
+
+## Cómo funciona 🤖
+IDiscursom utiliza modelos de procesamiento de lenguaje natural (NLP) para analizar archivos de texto. Incluye un endpoint para obtener si el texto es un discurso y un análisis detallado que incluye los textos más similares.
+
+## Endpoint🔚
+La API de IDiscursom tiene un endpoint principal:
+
+* POST /**analyze_text_type**: Este endpoint recibe un archivo de texto (.txt o .docx) y devuelve si el texto es un discurso, junto con la puntuación de similitud y los textos similares.
+
+## Uso 🖥️⌨️
+Para utilizar la API de IDiscursom, simplemente realiza solicitudes POST con el archivo de texto que deseas analizar. La API procesará el texto y te devolverá si el texto es un discurso y un análisis detallado.
+
+## Futuras mejoras 🚀
+En el futuro, planeamos expandir la funcionalidad para analizar textos en diferentes idiomas y géneros.
+
+También, se planea implementar funcionalidades específicas para analizar la estructura de los discursos.
+
+## Contribuciones 🌀
+Las contribuciones son bienvenidas. Si tienes alguna sugerencia o mejora, no dudes en abrir un problema o hacer un pull request.
+
+## Autores:✒️
+* **Gabriel Neme** -[GaboRex](https://github.com/GaboRex)
+**56189**
+
+* **Pablo Badani** -[Pabdro](https://github.com/Pabdro)
+**55789**
--- disAnalyzer/main.py
+++ disAnalyzer/main.py
@@ -1,54 +1,48 @@
 from fastapi import FastAPI, File, UploadFile
 from starlette.responses import JSONResponse
-from pydantic import BaseModel
 from sklearn.decomposition import PCA
-from sklearn.neighbors import NearestNeighbors
 from embeddings_loader import load_embeddings, extract_embedding
 from sklearn.metrics.pairwise import cosine_similarity
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
 from sklearn.decomposition import PCA
 from sklearn.metrics.pairwise import cosine_similarity
 from sqlmodel import Field, SQLModel, create_engine, Session
+from docx import Document
 
-# Define el modelo de datos para la tabla en la base de datos
 class TextEntry(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     text: str
     text_type: str
     similarity_score: float
 
-# Configura la base de datos SQLite
 DATABASE_URL = "sqlite:///db.sqlite3"
 engine = create_engine(DATABASE_URL)
 
-# Crea las tablas en la base de datos
 SQLModel.metadata.create_all(engine)
 
 app = FastAPI(title="Detector de Tipos de Texto")
 
 texts, embeddings, nlp, discursos, canciones, noticias = load_embeddings()
 
-# Realizar PCA para reducción de dimensionalidad
 pca = PCA(n_components=3)
 embeddings_3d = pca.fit_transform(embeddings)
 
 @app.post("/analyze_text_type")
 async def analyze_text_type(file: UploadFile = File(...)):
-    text = await file.read()
-    text = text.decode("utf-8")
+    if file.filename.endswith('.docx'):
+        doc = Document(file.file)
+        text = ' '.join([paragraph.text for paragraph in doc.paragraphs])
+    else:
+        text = await file.read()
+        text = text.decode("utf-8")
 
-    # Extraer el embedding del texto proporcionado
     text_embedding = extract_embedding(text, nlp).reshape(1, -1)
 
-    # Calcular la similitud coseno entre el texto y los embeddings precalculados
     similarity_scores = cosine_similarity(text_embedding, embeddings)
 
-    # Obtener los índices de los textos más similares
     indices_mas_similares = similarity_scores.argsort()[0][::-1]
 
-    # Determinar el tipo de texto más similar
     if indices_mas_similares[0] < len(canciones):
         text_type = "Cancion"
     elif indices_mas_similares[0] < len(canciones) + len(discursos):
@@ -56,10 +50,9 @@ async def analyze_text_type(file: UploadFile = File(...)):
     else:
         text_type = "Noticia"
 
-    # Devolver los resultados
     results = {
         "text_type": text_type,
-        "similarity_score": float(similarity_scores[0][indices_mas_similares[0]]),  # Convertir a float64
+        "similarity_score": float(similarity_scores[0][indices_mas_similares[0]]),  
         "similar_texts": [{"index": int(indice), "similarity": float(similarity_scores[0][indice])} for indice in indices_mas_similares[1:5]],  # Convertir a float64
     }
     db_text = TextEntry(
